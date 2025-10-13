7460862a08e7214165bdce4926a9374649f33006
Wilker Martins <dev.wilker@gmail.com>
2025-10-13 16:16:05 -0300

    layou

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-// src/app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer"; // Importe o Footer
+import Footer from "@/components/Footer"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
